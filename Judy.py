2b32e79244e2c449c5e2359869689810d1bfab8e
daniel <dan@dan.dan>
2022-02-20 05:48:47 +0100

    added new call-response, got rid of command signifier

--- Judy.py
+++ Judy.py
@@ -11,9 +11,13 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('hello'):
 
         await message.channel.send('Hello!')
+
+    if message.content.startswith('how are you'):
+
+        await message.channel.send('Not as good as your prison sentence.')
 
 client.run('OTQ0Nzk5MjE5MDk5NzY2ODc1.YhG21w.XJvkIYEGvxTUPMwgIxZ6icSvZzw')
 
